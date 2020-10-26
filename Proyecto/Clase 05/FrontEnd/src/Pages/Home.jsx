fea0f3d1f54563778b2f1462fc5f2739a638ff76
moitorres <moi.torres.m@gmail.com>
2020-10-26 17:36:27 -0600

    Added covid statistics chart

--- Proyecto/Clase 05/FrontEnd/src/Pages/Home.jsx
+++ Proyecto/Clase 05/FrontEnd/src/Pages/Home.jsx
@@ -1,11 +1,38 @@
 import React from "react";
 import CarouselHome from '../Components/CarouselHome';
+import Chart from "../Components/CovidCharts";
+import {CountryPicker, fetchData} from "../Components/CountryPicker";
 import '../App.css'
 
 class Home extends React.Component {
+
+
+  state = {
+    data: {},
+    country: "mx",
+  };
+
+  
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  };
+
   render() {
+    const { data, country } = this.state;
     return (
       <div>
+        <br/>
+        <h1 className="titulo">Estadísticas de covid</h1>
+        <br/>
+        <br/>
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+        <br/>
         <br/>
         <h1 className="titulo">Productos ofrecidos</h1>
         <br/>
